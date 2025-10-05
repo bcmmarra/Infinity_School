f8ae79532961a6616f4350a11db2190697ad8d80
Bruno Marra <bcm.marra@gmail.com>
2025-10-04 21:42:54 -0300

    Delete arquivos com erro

--- Modulo01_LP_Logica_de_Programacao/Aula01_Introducao_Algoritmos/Exercicios_Dunossauro/ex002.py
+++
@@ -1,7 +0,0 @@
-'''
-Exercício 02
-Faça um programa que peça um número e então mostre a mensagem "O número informado foi [número]":
-'''
-
-num = int(input('Digite um número: '))
-print(f'O npumero informado foi {num}')
